8b44f273bd010bfa060f22eb2bba4df08c517816
Jonathan Rainer <jr776@york.ac.uk>
2019-01-08 10:58:46 +0000

    Updating Custom IP to V1.2 to get rid of a function experiment

--- rtl/core2axi_wrapper.v
+++ rtl/core2axi_wrapper.v
@@ -160,12 +160,3 @@ module core2axi_wrapper
 	);
 
 endmodule
-
-function integer clog2;
-input integer value;
-begin
-value = value-1;
-for (clog2=0; value>0; clog2=clog2+1)
-value = value>>1;
-end
-endfunction
