39798e13c756e55b9433657065493e5bb0a0758a
Jonathan Rainer <jr776@york.ac.uk>
2019-02-19 10:07:47 +0000

    Removing CIP structure and sorting out scripts to account for the new way Kuuga works

---
+++ scripts/core2axi_manifest.tcl
@@ -0,0 +1,8 @@
+# Set up folders to be refered to later
+set core2AXIRTLRoot [file join $thisDir .. rtl]
+
+# Set up list of RTL files
+set core2AXIRTLFiles {
+    core2axi.sv
+    core2axi_wrapper.v
+}
